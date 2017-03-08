39d3feb1b99eb6b8fc8d5c85e4f21585f76cee76
davideabati <btadvd@gmail.com>
2017-03-08 16:32:56 +0100

    Added script to launch prediction on cineca
    Still TODO:
    handle it as a self-contained script, with no external deps like
    (computer vision utils etc)

--- experiments/predict_on_cineca/README.md
+++ experiments/predict_on_cineca/README.md
@@ -4,3 +4,7 @@ Code to run predictions of all sequences on cineca.
 to cineca, in order to use it to make predictions.
 
 Usage: `python move_dreyeve_of_to_cineca.py --host <cineca_host> --user <username> --password <password>`
+
+* `predict_dreyeve_sequence.py` script that calls prediction on all frames of a sequence.
+
+Usage: `python predict_dreyeve_sequence.py --seq <sequence_number> --pred_dir <prediction_dir>`
---
+++ experiments/predict_on_cineca/predict_dreyeve_sequence.py
@@ -0,0 +1,159 @@
+import numpy as np
+import cv2
+
+import argparse
+
+import os
+
+from os.path import join
+
+from models import DreyeveNet
+from computer_vision_utils.io_helper import read_image, normalize
+from computer_vision_utils.tensor_manipulation import resize_tensor
+from computer_vision_utils.stitching import stitch_together
+
+from utils import seg_to_colormap
+
+
+def makedirs(dir_list):
+    """
+    Helper function to create a list of directories.
+
+    :param dir_list: a list of directories to be created
+    """
+
+    for dir in dir_list:
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+
+
+def load_dreyeve_sample(sequence_dir, sample, mean_dreyeve_image, frames_per_seq=16, h=448, w=448, ):
+    """
+    Function to load a dreyeve_sample.
+
+    :param sequence_dir: string, sequence directory (e.g. 'Z:/DATA/04/').
+    :param sample: int, sample to load in (15, 7499). N.B. this is the sample where prediction occurs!
+    :param mean_dreyeve_image: mean dreyeve image, subtracted to each frame.
+    :param frames_per_seq: number of temporal frames for each sample
+    :param h: h
+    :param w: w
+    :return: a dreyeve_sample like I, OF, SEG
+    """
+
+    h_c = h_s = h // 4
+    w_c = w_s = h // 4
+
+    I_ff = np.zeros(shape=(1, 3, 1, h, w), dtype='float32')
+    I_s = np.zeros(shape=(1, 3, frames_per_seq, h_s, w_s), dtype='float32')
+    I_c = np.zeros(shape=(1, 3, frames_per_seq, h_c, w_c), dtype='float32')
+    OF_ff = np.zeros(shape=(1, 3, 1, h, w), dtype='float32')
+    OF_s = np.zeros(shape=(1, 3, frames_per_seq, h_s, w_s), dtype='float32')
+    OF_c = np.zeros(shape=(1, 3, frames_per_seq, h_c, w_c), dtype='float32')
+    SEG_ff = np.zeros(shape=(1, 19, 1, h, w), dtype='float32')
+    SEG_s = np.zeros(shape=(1, 19, frames_per_seq, h_s, w_s), dtype='float32')
+    SEG_c = np.zeros(shape=(1, 19, frames_per_seq, h_c, w_c), dtype='float32')
+
+    for fr in xrange(0, frames_per_seq):
+        offset = sample - frames_per_seq + 1 + fr   # tricky
+
+        # read image
+        x = read_image(join(sequence_dir, 'frames', '{:06d}.jpg'.format(offset)),
+                       channels_first=True, resize_dim=(h, w)) - mean_dreyeve_image
+        I_s[0, :, fr, :, :] = resize_tensor(x, new_size=(h_s, w_s))
+
+        # read of
+        of = read_image(join(sequence_dir, 'optical_flow', '{:06d}.png'.format(offset + 1)),
+                        channels_first=True, resize_dim=(h, w))
+        of -= np.mean(of, axis=(1, 2), keepdims=True)  # remove mean
+        OF_s[0, :, fr, :, :] = resize_tensor(of, new_size=(h_s, w_s))
+
+        # read semseg
+        seg = resize_tensor(np.load(join(sequence_dir, 'semseg', '{:06d}.npz'.format(offset)))['arr_0'][0],
+                            new_size=(h, w))
+        SEG_s[0, :, fr, :, :] = resize_tensor(seg, new_size=(h_s, w_s))
+
+    I_ff[0, :, 0, :, :] = x
+    OF_ff[0, :, 0, :, :] = of
+    SEG_ff[0, :, 0, :, :] = seg
+
+    return [I_ff, I_s, I_c, OF_ff, OF_s, OF_c, SEG_ff, SEG_s, SEG_c]
+
+
+if __name__ == '__main__':
+
+    frames_per_seq, h, w = 16, 448, 448
+    verbose = True
+
+    # parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--seq")
+    parser.add_argument("--pred_dir")
+    args = parser.parse_args()
+
+    assert args.seq is not None, 'Please provide a correct dreyeve sequence'
+    assert args.pred_dir is not None, 'Please provide a correct pred_dir'
+
+    dreyeve_dir = 'Z:/DATA'  # local
+    # dreyeve_dir = '/gpfs/work/IscrC_DeepVD/dabati/DREYEVE/data/'  # cineca
+
+    # load mean dreyeve image
+    mean_dreyeve_image = read_image(join(dreyeve_dir, 'dreyeve_mean_frame.png'),
+                                    channels_first=True, resize_dim=(h, w))
+
+    # get the models
+    dreyevenet_model = DreyeveNet(frames_per_seq=frames_per_seq, h=h, w=w)
+    dreyevenet_model.compile(optimizer='adam', loss='kld')  # do we need this?
+    dreyevenet_model.load_weights('model.h5')  # load weights
+
+    image_branch = [l for l in dreyevenet_model.layers if l.name == 'image_saliency_branch'][0]
+    flow_branch = [l for l in dreyevenet_model.layers if l.name == 'optical_flow_saliency_branch'][0]
+    semseg_branch = [l for l in dreyevenet_model.layers if l.name == 'segmentation_saliency_branch'][0]
+
+    # this is tmp
+    image_branch.load_weights('image_branch.h5')
+
+    # set up some directories
+    dreyevenet_pred_dir = join(args.pred_dir, '{:02d}'.format(int(args.seq)), 'dreyeveNet')
+    image_pred_dir = join(args.pred_dir, '{:02d}'.format(int(args.seq)), 'image_branch')
+    flow_pred_dir = join(args.pred_dir, '{:02d}'.format(int(args.seq)), 'flow_branch')
+    semseg_pred_dir = join(args.pred_dir, '{:02d}'.format(int(args.seq)), 'semseg_branch')
+    makedirs([dreyevenet_pred_dir, image_pred_dir, flow_pred_dir, semseg_pred_dir])
+
+    sequence_dir = join(dreyeve_dir, '{:02d}'.format(int(args.seq)))
+    for sample in xrange(15, 7500 - 1):
+        X = load_dreyeve_sample(sequence_dir=sequence_dir, sample=sample, mean_dreyeve_image=mean_dreyeve_image,
+                                frames_per_seq=frames_per_seq, h=h, w=w)
+
+        Y_dreyevenet = dreyevenet_model.predict(X)[0][0]  # get only [fine_out][remove batch]
+        Y_image = image_branch.predict(X[:3])[0][0]  # predict on image
+        Y_flow = flow_branch.predict(X[3:6])[0][0]  # predict on optical flow
+        Y_semseg = semseg_branch.predict(X[6:])[0][0]  # predict on segmentation
+
+        # save model output
+        np.savez_compressed(join(dreyevenet_pred_dir, '{:06d}'.format(sample)), Y_dreyevenet)
+        np.savez_compressed(join(image_pred_dir, '{:06d}'.format(sample)), Y_image)
+        np.savez_compressed(join(flow_pred_dir, '{:06d}'.format(sample)), Y_flow)
+        np.savez_compressed(join(semseg_pred_dir, '{:06d}'.format(sample)), Y_semseg)
+
+        if verbose:
+            # visualization
+            x_stitch = stitch_together([normalize(X[0][0, :, 0, :, :].transpose(1, 2, 0)),
+                                        normalize(X[3][0, :, 0, :, :].transpose(1, 2, 0)),
+                                        normalize(seg_to_colormap(np.argmax(X[6][0, :, 0, :, :], axis=0),
+                                                                  channels_first=False))],
+                                       layout=(3, 1), resize_dim=(720, 720))
+
+            y_stitch = stitch_together([np.tile(normalize(Y_image.transpose(1, 2, 0)), reps=(1, 1, 3)),
+                                        np.tile(normalize(Y_flow.transpose(1, 2, 0)), reps=(1, 1, 3)),
+                                        np.tile(normalize(Y_semseg.transpose(1, 2, 0)), reps=(1, 1, 3))],
+                                       layout=(3, 1), resize_dim=(720, 720))
+
+            y_tot = np.tile(normalize(resize_tensor(Y_dreyevenet, new_size=(720, 720)).transpose(1, 2, 0)),
+                            reps=(1, 1, 3))
+
+            cv2.imshow('prediction', stitch_together([x_stitch, y_stitch, y_tot], layout=(1, 3),
+                                                     resize_dim=(500, 1500)))
+            cv2.waitKey(1)
+
+
+
