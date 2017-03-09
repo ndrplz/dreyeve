62780217943d8ea5cf18c14d544712f9e52d8d0e
davideabati <btadvd@gmail.com>
2017-03-09 15:33:20 +0100

    Modified predict script to compute some metrics
    Now the script computes KLD and CC and saves them on file.

---
+++ experiments/predict_on_cineca/metrics.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+
+def kld_numeric(y_true, y_pred):
+    """
+    Function to evaluate Kullback-Leiber divergence (sec 4.2.3 of [1]) on two samples.
+
+    :param y_true: groundtruth, having shape (1, 1, h, w)
+    :param y_pred: predictions, having shape (1, 1, h, w)
+    :return: numeric kld
+    """
+    eps = np.finfo(float).eps
+
+    P = y_pred / (eps + np.sum(y_pred))  # prob
+    Q = y_true / (eps + np.sum(y_true))  # prob
+
+    kld = np.sum(Q * np.log(eps + Q / (eps + P)))
+
+    return kld
+
+
+def cc_numeric(y_true, y_pred):
+    """
+    Function to evaluate Pearson's correlation coefficient (sec 4.2.2 of [1]) on two samples.
+
+    :param y_true: groundtruth, having shape (1, 1, h, w)
+    :param y_pred: predictions, having shape (1, 1, h, w)
+    :return: numeric cc
+    """
+    eps = np.finfo(float).eps
+
+    P = y_pred / (eps + np.sum(y_pred))  # prob
+    Q = y_true / (eps + np.sum(y_true))  # prob
+
+    n = P.size
+    cc = np.dot(P.flatten() - np.mean(P), Q.flatten() - np.mean(Q)) / (np.std(P) * np.std(Q)) / n
+
+    return cc
+
+"""
+REFERENCES:
+[1] @article{salMetrics_Bylinskii,
+  title     = {What do different evaluation metrics tell us about saliency models?},
+  author    = {Zoya Bylinskii and Tilke Judd and Aude Oliva and Antonio Torralba and Fr{\'e}do Durand},
+  journal   = {arXiv preprint arXiv:1604.03605},
+  year      = {2016}
+}
+"""
--- experiments/predict_on_cineca/predict_dreyeve_sequence.py
+++ experiments/predict_on_cineca/predict_dreyeve_sequence.py
@@ -4,7 +4,7 @@ import cv2
 import argparse
 
 import os
-
+import tqdm
 from os.path import join
 
 from models import DreyeveNet
@@ -13,6 +13,7 @@ from computer_vision_utils.tensor_manipulation import resize_tensor
 from computer_vision_utils.stitching import stitch_together
 
 from utils import seg_to_colormap
+from metrics import kld_numeric, cc_numeric
 
 
 def makedirs(dir_list):
@@ -53,6 +54,9 @@ def load_dreyeve_sample(sequence_dir, sample, mean_dreyeve_image, frames_per_seq
     SEG_s = np.zeros(shape=(1, 19, frames_per_seq, h_s, w_s), dtype='float32')
     SEG_c = np.zeros(shape=(1, 19, frames_per_seq, h_c, w_c), dtype='float32')
 
+    Y_sal = np.zeros(shape=(1, 1, h, w), dtype='float32')
+    Y_fix = np.zeros(shape=(1, 1, h, w), dtype='float32')
+
     for fr in xrange(0, frames_per_seq):
         offset = sample - frames_per_seq + 1 + fr   # tricky
 
@@ -76,13 +80,18 @@ def load_dreyeve_sample(sequence_dir, sample, mean_dreyeve_image, frames_per_seq
     OF_ff[0, :, 0, :, :] = of
     SEG_ff[0, :, 0, :, :] = seg
 
-    return [I_ff, I_s, I_c, OF_ff, OF_s, OF_c, SEG_ff, SEG_s, SEG_c]
+    Y_sal[0, 0] = read_image(join(sequence_dir, 'saliency', '{:06d}.png'.format(sample)), channels_first=False,
+                             color=False, resize_dim=(h, w))
+    Y_fix[0, 0] = read_image(join(sequence_dir, 'saliency_fix', '{:06d}.png'.format(sample)), channels_first=False,
+                             color=False, resize_dim=(h, w))
+
+    return [I_ff, I_s, I_c, OF_ff, OF_s, OF_c, SEG_ff, SEG_s, SEG_c], [Y_sal, Y_fix]
 
 
 if __name__ == '__main__':
 
     frames_per_seq, h, w = 16, 448, 448
-    verbose = True
+    verbose = False
 
     # parse arguments
     parser = argparse.ArgumentParser()
@@ -117,20 +126,45 @@ if __name__ == '__main__':
     makedirs([dreyevenet_pred_dir, image_pred_dir, flow_pred_dir, semseg_pred_dir])
 
     sequence_dir = join(dreyeve_dir, '{:02d}'.format(int(args.seq)))
-    for sample in xrange(15, 7500 - 1):
-        X = load_dreyeve_sample(sequence_dir=sequence_dir, sample=sample, mean_dreyeve_image=mean_dreyeve_image,
+    for sample in tqdm(range(15, 7500 - 1)):
+        X, GT = load_dreyeve_sample(sequence_dir=sequence_dir, sample=sample, mean_dreyeve_image=mean_dreyeve_image,
                                 frames_per_seq=frames_per_seq, h=h, w=w)
+        GT_sal, GT_fix = GT
 
-        Y_dreyevenet = dreyevenet_model.predict(X)[0][0]  # get only [fine_out][remove batch]
-        Y_image = image_branch.predict(X[:3])[0][0]  # predict on image
-        Y_flow = flow_branch.predict(X[3:6])[0][0]  # predict on optical flow
-        Y_semseg = semseg_branch.predict(X[6:])[0][0]  # predict on segmentation
+        Y_dreyevenet = dreyevenet_model.predict(X)[0]  # get only [fine_out][remove batch]
+        Y_image = image_branch.predict(X[:3])[0]  # predict on image
+        Y_flow = flow_branch.predict(X[3:6])[0]  # predict on optical flow
+        Y_semseg = semseg_branch.predict(X[6:])[0]  # predict on segmentation
 
         # save model output
         np.savez_compressed(join(dreyevenet_pred_dir, '{:06d}'.format(sample)), Y_dreyevenet)
         np.savez_compressed(join(image_pred_dir, '{:06d}'.format(sample)), Y_image)
         np.savez_compressed(join(flow_pred_dir, '{:06d}'.format(sample)), Y_flow)
         np.savez_compressed(join(semseg_pred_dir, '{:06d}'.format(sample)), Y_semseg)
+
+        # save some metrics
+        with open(join(args.pred_dir, '{:02d}'.format(int(args.seq)), 'kld.txt'), 'a') as metric_file:
+            metric_file.write('{},{},{},{},{},{},{},{},{}\n'.format(sample,
+                                                                    kld_numeric(GT_sal, Y_dreyevenet),
+                                                                    kld_numeric(GT_fix, Y_dreyevenet),
+                                                                    kld_numeric(GT_sal, Y_image),
+                                                                    kld_numeric(GT_fix, Y_image),
+                                                                    kld_numeric(GT_sal, Y_flow),
+                                                                    kld_numeric(GT_fix, Y_flow),
+                                                                    kld_numeric(GT_sal, Y_semseg),
+                                                                    kld_numeric(GT_fix, Y_semseg),
+                                                                    ))
+        with open(join(args.pred_dir, '{:02d}'.format(int(args.seq)), 'cc.txt'), 'a') as metric_file:
+            metric_file.write('{},{},{},{},{},{},{},{},{}\n'.format(sample,
+                                                                    cc_numeric(GT_sal, Y_dreyevenet),
+                                                                    cc_numeric(GT_fix, Y_dreyevenet),
+                                                                    cc_numeric(GT_sal, Y_image),
+                                                                    cc_numeric(GT_fix, Y_image),
+                                                                    cc_numeric(GT_sal, Y_flow),
+                                                                    cc_numeric(GT_fix, Y_flow),
+                                                                    cc_numeric(GT_sal, Y_semseg),
+                                                                    cc_numeric(GT_fix, Y_semseg),
+                                                                    ))
 
         if verbose:
             # visualization
@@ -140,12 +174,12 @@ if __name__ == '__main__':
                                                                   channels_first=False))],
                                        layout=(3, 1), resize_dim=(720, 720))
 
-            y_stitch = stitch_together([np.tile(normalize(Y_image.transpose(1, 2, 0)), reps=(1, 1, 3)),
-                                        np.tile(normalize(Y_flow.transpose(1, 2, 0)), reps=(1, 1, 3)),
-                                        np.tile(normalize(Y_semseg.transpose(1, 2, 0)), reps=(1, 1, 3))],
+            y_stitch = stitch_together([np.tile(normalize(Y_image[0].transpose(1, 2, 0)), reps=(1, 1, 3)),
+                                        np.tile(normalize(Y_flow[0].transpose(1, 2, 0)), reps=(1, 1, 3)),
+                                        np.tile(normalize(Y_semseg[0].transpose(1, 2, 0)), reps=(1, 1, 3))],
                                        layout=(3, 1), resize_dim=(720, 720))
 
-            y_tot = np.tile(normalize(resize_tensor(Y_dreyevenet, new_size=(720, 720)).transpose(1, 2, 0)),
+            y_tot = np.tile(normalize(resize_tensor(Y_dreyevenet[0], new_size=(720, 720)).transpose(1, 2, 0)),
                             reps=(1, 1, 3))
 
             cv2.imshow('prediction', stitch_together([x_stitch, y_stitch, y_tot], layout=(1, 3),
