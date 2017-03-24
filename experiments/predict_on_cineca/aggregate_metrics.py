6af92c41cd3d84bc1a90fa9120341234cb13ece6
davideabati <btadvd@gmail.com>
2017-03-24 11:36:14 +0100

    Refactor the script to aggregate metrics

--- experiments/predict_on_cineca/aggregate_metrics.py
+++ experiments/predict_on_cineca/aggregate_metrics.py
@@ -1,25 +1,43 @@
 import numpy as np
 
+import os
 from os.path import join
-from glob import glob
+
 
 if __name__ == '__main__':
 
-    predictions_dir = 'F:\DREYEVE\PREDICTIONS_2017'
+    # do not change these
+    sequences_train = xrange(1, 38 + 1)
+    sequences_test = xrange(39, 74 + 1)
+    predictions_dir = 'Z:/PREDICTIONS/architecture7'
 
-    files_to_merge = ['metrics/kld_mean.txt', 'metrics/cc_mean.txt', 'ablation/kld_mean.txt', 'ablation/cc_mean.txt']
+    # change these
+    metrics_to_merge = ['metrics/kld_mean.txt', 'metrics/cc_mean.txt', 'metrics/ig_mean.txt']
+    mode = 'test'
 
-    for fm in files_to_merge:
+    assert mode in ['train', 'test'], 'Non valid mode: {}'.format(mode)
+
+    sequences = sequences_train if mode == 'train' else sequences_test
+
+    for metric_filename in metrics_to_merge:
         metrics = []
         header = ''
-        for i in glob(join(predictions_dir, '**', fm)):
-            with open(i) as f:
+        # read metric for all sequences
+        for metric_file in [join(predictions_dir, '{:02d}'.format(seq), metric_filename) for seq in sequences]:
+            # read
+            with open(metric_file) as f:
                 lines = f.readlines()
+            # read header
             header = lines[0]
-            numbers = [s.split(',') for s in lines[1:]]
-            metrics.append(numbers[0])
+            # read numbers
+            numbers = [s.split(',') for s in lines[1:]][0]
+            numbers = [n for n in numbers if n != '']
+            metrics.append(numbers)
+        # mean on all sequences
         means = np.mean(np.array(metrics, dtype=np.float32), axis=0, keepdims=False)
-        with open(join(predictions_dir, fm.replace('/', '_')), mode='w') as f:
+        # write
+        with open(join(predictions_dir, '{}_{}'.format(mode, os.path.basename(metric_filename))),
+                  mode='w') as f:
             f.write(header)
             f.write(('{},'*means.size).format(*list(means)))
 
