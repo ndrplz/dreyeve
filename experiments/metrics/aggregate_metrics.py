a71eb52fa6ea4a69494747608b0f5649ce962d72
davideabati <btadvd@gmail.com>
2017-04-01 13:31:38 +0200

    Fix bug in aggregate metrics
    Now the mean is weighted according to the number of frames composing it

--- experiments/metrics/aggregate_metrics.py
+++ experiments/metrics/aggregate_metrics.py
@@ -2,6 +2,14 @@ import numpy as np
 
 import os
 from os.path import join
+from glob import glob
+
+
+def file_len(fname):
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
 
 
 if __name__ == '__main__':
@@ -9,12 +17,11 @@ if __name__ == '__main__':
     # do not change these
     sequences_train = xrange(1, 38 + 1)
     sequences_test = xrange(39, 74 + 1)
-    predictions_dir = 'Z:/PREDICTIONS_2017'
+    predictions_dir = 'Z:/PREDICTIONS_wang2015saliency'
 
     # change these
-    metrics_to_merge = ['metrics/kld_mean.txt', 'metrics/cc_mean.txt', 'metrics/ig_mean.txt',
-                        'ablation/kld_mean.txt', 'ablation/cc_mean.txt', 'ablation/ig_mean.txt']
-    mode = 'only_good_semseg'
+    metrics_to_merge = ['metrics/kld_mean.txt', 'metrics/cc_mean.txt', 'metrics/ig_mean.txt']
+    mode = 'test'
 
     assert mode in ['train', 'test', 'only_good_semseg'], 'Non valid mode: {}'.format(mode)
 
@@ -29,8 +36,13 @@ if __name__ == '__main__':
     for metric_filename in metrics_to_merge:
         metrics = []
         header = ''
+        frames = 0
         # read metric for all sequences
         for metric_file in [join(predictions_dir, '{:02d}'.format(seq), metric_filename) for seq in sequences]:
+            # count how many frames we have in this sequence
+            # pardon, this part was added after
+            cur_frames = file_len(metric_file.replace('_mean', '')) - 1
+
             # read
             with open(metric_file) as f:
                 lines = f.readlines()
@@ -38,10 +50,14 @@ if __name__ == '__main__':
             header = lines[0]
             # read numbers
             numbers = [s.split(',') for s in lines[1:]][0]
-            numbers = [n for n in numbers if n != '']
+            numbers = [float(n)*cur_frames for n in numbers if n != '']
+
             metrics.append(numbers)
+            frames += cur_frames
+
         # mean on all sequences
-        means = np.mean(np.array(metrics, dtype=np.float32), axis=0, keepdims=False)
+        means = np.sum(np.array(metrics, dtype=np.float32), axis=0, keepdims=False)
+        means /= frames
         # write
         with open(join(predictions_dir, '{}_{}'.format(mode, metric_filename.replace('/', '_'))),
                   mode='w') as f:
