45fb46ba01f1a3af94431e54661108c7c517e997
davideabati <btadvd@gmail.com>
2017-04-03 11:49:54 +0200

    Fix in compute_metrics_on_attentive
    Now the script computes also information gain

--- experiments/metrics/compute_metrics_on_attentive.py
+++ experiments/metrics/compute_metrics_on_attentive.py
@@ -12,8 +12,7 @@ if __name__ == '__main__':
         csvreader = csv.reader(csv_subseq, delimiter='\t')
         subsequences = list(csvreader)
 
-    cc_f_by_f,  kl_f_by_f   = [], []
-    cc_all,     kl_all      = [], []
+    cc_all, kl_all, ig_all = [], [], []
 
     prediction_root = 'Z:/PREDICTIONS_2017'
     test_runs_measures = [(r, join(prediction_root, '{:02d}'.format(r))) for r in range(38, 75)]
@@ -30,6 +29,11 @@ if __name__ == '__main__':
             headers = reader.next()  # store headers
             kl_f_by_f = {int(row[0]): np.array(row[1:-1], dtype=np.float32) for row in reader}
 
+        with open(join(run_dir, 'metrics', 'ig.txt'), 'r') as f:
+            reader = csv.reader(f)
+            headers = reader.next()  # store headers
+            ig_f_by_f = {int(row[0]): np.array(row[1:-1], dtype=np.float32) for row in reader}
+
         # make a list with all the frames of this run marked as 'keep'
         # each row of `keep_this_run` is a list of four entries [num_run, start_frame, end_frame, subsequence_class]
         keep_this_run = [l for l in subsequences if l[0] == '{0}'.format(r) and l[3] == 'k']
@@ -41,23 +45,26 @@ if __name__ == '__main__':
         # keep only metrics for frames in the selected list (these are frames marked as attentive)
         filtered_cc = np.array([cc_values for key, cc_values in cc_f_by_f.iteritems() if key in frames_keep_this_run])
         filtered_kl = np.array([kld_values for key, kld_values in kl_f_by_f.iteritems() if key in frames_keep_this_run])
+        filtered_ig = np.array([ig_values for key, ig_values in ig_f_by_f.iteritems() if key in frames_keep_this_run])
 
         mean_cc_attentive_this_run = np.nanmean(filtered_cc, axis=0)
         mean_kl_attentive_this_run = np.nanmean(filtered_kl, axis=0)
+        mean_ig_attentive_this_run = np.nanmean(filtered_ig, axis=0)
 
         cc_all.append(mean_cc_attentive_this_run)
         kl_all.append(mean_kl_attentive_this_run)
+        ig_all.append(mean_ig_attentive_this_run)
 
     # at the end, average results over all test runs
     mean_all_cc_attentive = np.mean(np.array(cc_all), axis=0)
     mean_all_kl_attentive = np.mean(np.array(kl_all), axis=0)
+    mean_all_ig_attentive = np.mean(np.array(ig_all), axis=0)
 
-    print('CC_DREYEVE_WRT_SAL, CC_IMAGE_WRT_SAL, CC_FLOW_WRT_SAL, CC_SEG_WRT_SAL')
-    print(mean_all_cc_attentive[:4])
-    print('CC_DREYEVE_WRT_FIX, CC_IMAGE_WRT_FIX, CC_FLOW_WRT_FIX, CC_SEG_WRT_FIX')
-    print(mean_all_cc_attentive[4:])
+    print('CC')
+    print(mean_all_cc_attentive)
     print('')
-    print('KLD_DREYEVE_WRT_SAL,KLD_IMAGE_WRT_SAL,KLD_FLOW_WRT_SAL,KLD_SEG_WRT_SAL')
-    print(mean_all_kl_attentive[:4])
-    print('KLD_DREYEVE_WRT_FIX,KLD_IMAGE_WRT_FIX,KLD_FLOW_WRT_FIX,KLD_SEG_WRT_FIX')
-    print(mean_all_kl_attentive[4:])
+    print('KLD')
+    print(mean_all_kl_attentive)
+    print('')
+    print('IG')
+    print(mean_all_ig_attentive)
