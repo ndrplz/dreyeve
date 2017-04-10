b19e4e7d1897823676ef05fdda85827a909af5e0
davideabati <btadvd@gmail.com>
2017-04-10 15:29:25 +0200

    Add batch generator for training

---
+++ experiments/rmdn_comparison/batch_generators.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+from computer_vision_utils.io_helper import read_image
+
+from os.path import join
+
+from config import DREYEVE_ROOT
+from config import dreyeve_train_seq, dreyeve_test_seq
+from config import train_frame_range, val_frame_range, test_frame_range
+from config import T, h, w, encoding_dim
+
+
+def RMDN_batch(batchsize, mode):
+    assert mode in ['train', 'val', 'test'], 'Unknown mode {} for dreyeve batch loader'.format(mode)
+
+    if mode == 'train':
+        sequences = dreyeve_train_seq
+        allowed_frames = train_frame_range
+    elif mode == 'val':
+        sequences = dreyeve_train_seq
+        allowed_frames = val_frame_range
+    elif mode == 'test':
+        sequences = dreyeve_test_seq
+        allowed_frames = test_frame_range
+
+    X = np.zeros(shape=(batchsize, T, encoding_dim), dtype=np.float32)
+    Y = np.zeros(shape=(batchsize, T, h, w))
+    for b in xrange(0, batchsize):
+        # sample a sequence
+        seq = np.random.choice(sequences)
+        seq_enc_dir = join(DREYEVE_ROOT, '{:02d}'.format(seq), 'c3d_encodings')
+        seq_gt_dir = join(DREYEVE_ROOT, '{:02d}'.format(seq), 'saliency_fix')
+
+        # choose a random sample
+        fr = np.random.choice(allowed_frames)
+        for offset in range(0, T):
+            c3d_encoding = np.load(join(seq_enc_dir, '{:06d}.npz'.format(fr+offset)))['arr_0']
+            gt = read_image(join(seq_gt_dir, '{:06d}.png'.format(fr+offset+1)), channels_first=False, color=False,
+                            resize_dim=(h, w))
+
+            X[b, offset] = c3d_encoding
+            Y[b, offset] = gt
+
+    return X, Y
+
+if __name__ == '__main__':
+    X, Y = RMDN_batch(batchsize=8, mode='train')
--- experiments/rmdn_comparison/config.py
+++ experiments/rmdn_comparison/config.py
@@ -1,7 +1,17 @@
 DREYEVE_ROOT = 'Z:/DATA'  # local
 # DREYEVE_ROOT = '/gpfs/work/IscrC_DeepVD/dabati/DREYEVE/data/'  # cineca
+frames_per_seq = 16
+total_frames_each_run = 7500
+encoding_dim = 81920
+
 h, w = 128, 171
 T = 50
 C = 20
-
 batchsize = 32
+
+dreyeve_train_seq = range(1, 37+1)
+dreyeve_test_seq = range(38, 74+1)
+train_frame_range = range(15, 3500 - T) + range(4000, total_frames_each_run - T)
+val_frame_range = range(3500, 4000 - T)
+test_frame_range = range(0, total_frames_each_run - T)
+
